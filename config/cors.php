6ff420f6928113fd5d3e83261aa817d860f408c5
seo-ben <sewodakomla@gmail.com>
2026-03-29 22:07:16 +0200

    correction du corp

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://omegapos.netlify.app')],
 
     'allowed_origins_patterns' => [],
 
